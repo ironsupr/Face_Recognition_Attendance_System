20f3e27a26f3b766f8c0d811e0a95f1b29630446
ironsupr <abhinavsingh25001@gmail.com>
2025-02-04 11:19:29 +0530

    Added UI to Student data

--- GUI/show_std_data.py
+++ GUI/show_std_data.py
@@ -1,11 +1,24 @@
 from pymongo import MongoClient
+from customtkinter import *
 try:
-    MONGO_URI = ""# Your MongoDB URI
+    # MONGO_URI = ""# Your MongoDB URI
     client = MongoClient(MONGO_URI)
 
     client.admin.command("ping")
     print("Connected successfully")
-    # other application code
+
+    db = client["student_data"]
+    collection = db["students"]
+
+    root = CTk()
+    root.title("Student Data")
+    root.geometry("1000x1200")
+
+    label_title = CTkLabel(root, text="Student Data", font=("Arial", 30))
+    label_title.place(x=10, y = 1)
+
+    root.mainloop()
+
     client.close()
 except Exception as e:
     raise Exception(
