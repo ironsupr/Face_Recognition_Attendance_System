74f6e55e734ec3b32d664a948e922260a2a53ec1
ironsupr <abhinavsingh25001@gmail.com>
2025-02-05 23:55:52 +0530

    Added buttons

--- GUI/show_std_data.py
+++ GUI/show_std_data.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from customtkinter import *
 try:
     # MONGO_URI = ""# Your MongoDB URI
+    
     client = MongoClient(MONGO_URI)
 
     client.admin.command("ping")
@@ -12,10 +13,13 @@ try:
 
     root = CTk()
     root.title("Student Data")
-    root.geometry("1000x1200")
+    root.geometry("1200x800")
 
     label_title = CTkLabel(root, text="Student Data", font=("Arial", 30))
     label_title.place(x=10, y = 1)
+
+    label_name = CTkLabel(root, text = "Enter Name", font=("Arial", 20))
+    label_name.place(x=400, y=200)
 
     root.mainloop()
 
